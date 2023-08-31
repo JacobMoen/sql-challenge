1d429d60481e7bb78a0f6e92f46b350a5a148cee
JacobMoen <134341713+JacobMoen@users.noreply.github.com>
2023-08-30 20:35:06 -0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -11,20 +11,20 @@ WHERE hire_date BETWEEN '1986-1-1' and '1986-12-31'
 ORDER BY hire_date ASC;
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
-SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
-FROM dept_manager dm
-JOIN employees e
-ON dm.emp_no = e.emp_no
-JOIN departments d
+SELECT dm.dept_no, d.dept_name, dm.emp_no, emp.last_name, emp.first_name 
+FROM dept_manager AS dm
+JOIN employees AS emp
+ON dm.emp_no = emp.emp_no
+JOIN departments AS d
 ON dm.dept_no = d.dept_no
 ORDER BY d.dept_name ASC;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees e
-JOIN dept_emp de 
-ON e.emp_no = de.emp_no
-JOIN departments d
+SELECT emp.emp_no, emp.last_name, emp.first_name, d.dept_name
+FROM employees AS emp
+JOIN dept_emp AS de 
+ON emp.emp_no = de.emp_no
+JOIN departments AS d
 ON d.dept_no = de.dept_no
 ORDER BY d.dept_name ASC;
 
@@ -32,23 +32,23 @@ ORDER BY d.dept_name ASC;
 SELECT first_name, last_name, sex
 FROM employees 
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
-ORDER BY last_name ASC
+ORDER BY last_name ASC;
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees e
-JOIN dept_emp de 
-ON e.emp_no = de.emp_no
-JOIN departments d
+SELECT emp.emp_no, emp.last_name, emp.first_name, d.dept_name
+FROM employees AS emp
+JOIN dept_emp AS de 
+ON emp.emp_no = de.emp_no
+JOIN departments AS d
 ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
-select emp.emp_no, emp.last_name, emp.first_name, dept.dept_name
-from employees as emp
-inner join dept_emp on dept_emp.emp_no = emp.emp_no
-inner join departments as dept on dept_emp.dept_no = dept.dept_no
-where dept.dept_name = 'Sales' or dept.dept_name = 'Developement'; 
+SELECT emp.emp_no, emp.last_name, emp.first_name, dept.dept_name
+FROM employees AS emp
+INNER JOIN dept_emp ON dept_emp.emp_no = emp.emp_no
+INNER JOIN departments AS dept ON dept_emp.dept_no = dept.dept_no
+WHERE dept.dept_name = 'Sales' OR dept.dept_name = 'Developement'; 
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, count(emp_no) as num_employees_with_same_last_name
